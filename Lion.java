db7465713dc10699969e41b508579d2664185d67
Tyler Dolph <tdolph0016@myduneland.org>
2020-09-14 08:43:10 -0500

    Initial sharing of project

---
+++ Lion.java
@@ -0,0 +1,6 @@
+public class Lion{
+    private int age;
+    public void roar(){
+    	System.out.println("Roar!  Roar!");
+    }
+}
---
+++ ZooProgram.java
@@ -0,0 +1,6 @@
+public class ZooProgram{
+    public static void main(String args[]){
+        Lion leo = new Lion();
+        leo.roar();
+    }
+}

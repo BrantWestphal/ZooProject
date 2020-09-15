d9ac6a816e49559f5b670601c64a681c631007d0
Gamingtrex5433 <55600002+Gamingtrex5433@users.noreply.github.com>
2020-09-15 09:28:28 -0500

    Add files via upload

---
+++ Snake.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Lion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Snake
+{
+    // instance variables - replace the example below with your own
+    private int age;
+
+    /**
+     * Constructor for objects of class Snake
+     */
+    public Snake()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void hiss()
+    {
+        // put your code here
+        System.out.println("Hiss!  Hiss!");
+
+    }
+}

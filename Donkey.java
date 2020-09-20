113536b46f7fd140d3c71957eb4f9f04651d7314
zvanderslice <62152014+zvanderslice@users.noreply.github.com>
2020-09-20 15:05:12 -0500

    Add files via upload

---
+++ Donkey.java
@@ -0,0 +1,21 @@
+
+/**
+ * Makes a Donkey Noise
+ *
+ * Zach Vanderslice
+ * 9/14/20
+ */
+public class Donkey
+{
+    private int age;
+    public void talk()
+    {
+        System.out.println("Heeawh!  Heeawh!");
+
+    } 
+    public void eat()
+    {
+        System.out.println("Ahhammp");
+
+    } 
+}

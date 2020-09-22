519379a9abbc1208ff456d2979064c8c15147960
Ms. O'Connell <soconnell@duneland.k12.in.us>
2020-09-22 10:00:24 -0500

    Added comments

--- ZooProgram.java
+++ ZooProgram.java
@@ -1,30 +1,31 @@
 public class ZooProgram{
     public static void main(String args[]){
         Lion leo = new Lion();
-	leo.roar();
-	    
-	Monkey monky = new Monkey();
-	monky.chimper();
-	    
+    leo.roar();
+        
+    Monkey monky = new Monkey();
+    monky.chimper();
+        
         Cat caico = new Cat();
         caico.meow();
-	    
+        
         Shark donny = new Shark ();
         donny.swim();
-	    
+        
         Sloth kevin = new Sloth();
         kevin.talk();
         kevin.eat();
-	    
-	Goat timothy = new Goat();
+        
+    Goat timothy = new Goat();
         timothy.eat();
         
-        Teacher msO = new Teacher();
+        Teacher msO = new Teacher();    //Ms. O added Teacher class and
+                                        //msO instance
         msO.teacherSays(0);
         msO.teacherSays(1);
         msO.teacherSays(4);
-	
-	Axolotl Ali = new Axolotl();
-	Ali.eats();
+    
+    Axolotl Ali = new Axolotl();
+    Ali.eats();
     }
 }

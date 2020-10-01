46ffd28bdc89319711b5ca2fce2518d390287a06
Ben-Shade <55599909+Ben-Shade@users.noreply.github.com>
2020-10-01 06:53:57 -0700

    Add files via upload

---
+++ Zoo-Project-Real/Turtle.java
@@ -0,0 +1,16 @@
+// Ben Shade
+// CS AP
+//turtle code for zoo project
+// 10/1/20
+public class Turtle
+{
+    public Turtle()
+    {
+    }
+
+    public void chank()
+    {
+        System.out.print("chank! chank!");
+    }
+}
+
---
+++ Zoo-Project-Real/ZooProject.java
@@ -0,0 +1,11 @@
+//Ben Shade
+// CS AP 
+// turtle code for zoo project
+//10/1/20
+public class ZooProject
+{
+   public static void main (String [] args) {
+       Turtle taytay = new Turtle();
+       taytay.chank();
+    }
+}

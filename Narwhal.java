5fdbc1368344d9da8c00f8d7b2ab4768cec4e926
BrantWestphal <71346998+BrantWestphal@users.noreply.github.com>
2020-10-06 12:05:59 -0500

    Create Narwhal.java

---
+++ Narwhal.java
@@ -0,0 +1,15 @@
+/**
+ * Write a description of class Narwhal here.
+ *
+ * @author (Brant Westphal)
+ * @version (10/6/2020)
+ */
+ public class Narwhal
+  {
+	  public int age;
+
+	  public void nyerr()
+	  {
+		      System.out.println("NYERRR, NYERRR");
+	} // nyerr() end
+} //Narwhal end
